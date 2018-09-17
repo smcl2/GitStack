5bef8007c2cfc9adbc0f9cf92103b9625d75cd2a
simon <simon@DESKTOP-KT9HR3L>
2018-09-17 12:53:20 +0100

    new branch stack_pop and implementation of stack pop

--- src/arraystack/ArrayStack.java
+++ src/arraystack/ArrayStack.java
@@ -1,5 +1,25 @@
 package arraystack;
 
 public class ArrayStack {
-
+	int [] stack;
+	int top;
+	public ArrayStack(int capacity)
+	{
+		stack = new int[capacity];
+		top = -1;
+	}
+	public boolean isEmpty()
+	{
+		return (top==-1);
+	}
+	public int pop()
+	{
+		int topItem = -1;
+		if (!isEmpty())
+		{
+			topItem = stack[top];
+		}
+		return topItem;
+	}
+	
 }
